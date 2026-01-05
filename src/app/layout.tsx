2c21c115b8f19091b3398d57c6f4a0530132502e
MaskSysDev <masksysdev@gmail.com>
2026-01-05 02:59:53 -0300

    feat: 02-clean-start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { geistMono, geistSans } from "@/lib/fonts";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,7 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      className="scroll-smooth"
+      data-scroll-behavior="smooth"
+      dir="ltr"
+      lang="en"
+    >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
