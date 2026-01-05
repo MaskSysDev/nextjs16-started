7e55a0fc7a3872c84294ea5b94333ae86e025197
MaskSysDev <masksysdev@gmail.com>
2026-01-05 03:12:48 -0300

    feat: 04-shadcn-ui

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { ToggleTheme } from "@/components/shared/toggle/toggle-theme";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center gap-2 p-24">
       <h1 className="font-bold text-3xl">NextJS 16 Started</h1>
+      <Button>Button</Button>
+      <ToggleTheme />
     </main>
   );
 }
